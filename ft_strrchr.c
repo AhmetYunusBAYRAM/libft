197c26514aaddc12eaed5cb01b05db88318e8c7e
Ahmet Yunus Bayram <abayram@k1m11s03.42kocaeli.com.tr>
2026-01-18 20:17:42 +0300

    ft_strrchar.c norm düzeltildi

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,25 +6,24 @@
 /*   By: abayram <abayram@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/18 20:03:25 by abayram           #+#    #+#             */
-/*   Updated: 2026/01/18 20:09:17 by abayram          ###   ########.fr       */
+/*   Updated: 2026/01/18 20:16:16 by abayram          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char *ft_strrchar(const char *word, int wanted)
+char	*ft_strrchar(const char *word, int wanted)
 {
 	int	length;
-	length = 0;
 
+	length = 0;
 	while (word[length] != '\0')
 	{
 		length++;
 	}
-
 	while (length >= 0)
 	{
-		if(word[length] == wanted)
+		if (word[length] == wanted)
 		{
-			return (&word[length]);
+			return ((char *)&word[length]);
 		}
 		length--;
 	}
