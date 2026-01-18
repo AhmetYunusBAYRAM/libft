78d7b1add0c07736fb1b42d025bda3b10c846985
Ahmet Yunus Bayram <abayram@k1m11s03.42kocaeli.com.tr>
2026-01-18 19:43:29 +0300

    ft_memcpy norm düzenlendi

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,25 +6,26 @@
 /*   By: abayram <abayram@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/17 17:43:10 by abayram           #+#    #+#             */
-/*   Updated: 2026/01/18 15:25:48 by abayram          ###   ########.fr       */
+/*   Updated: 2026/01/18 19:43:09 by abayram          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 
-void	*memcpy(void *target, const void *head, size_t count)
+void	*ft_memcpy(void *target, const void *head, size_t count)
 {
 	unsigned char	*char_target;
 	unsigned char	*char_head;
-	unsidged int	index;
 
+	if (head == NULL && target == NULL)
+	{
+		return (NULL);
+	}
 	char_target = (unsigned char *)target;
 	char_head = (unsigned char *)head;
-	index = 0;
-	while (index < count)
+	while (count--)
 	{
-		target[index] = head[index];
-		index++;
+		*char_target++ = *char_head++;
 	}
 	return (target);
 }
