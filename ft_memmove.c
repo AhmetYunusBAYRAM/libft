a4b06bdd5df6eda464bce4bd3010c14c862aa447
Ahmet Yunus Bayram <abayram@k1m13s04.42kocaeli.com.tr>
2026-01-20 21:07:41 +0300

    ft_memmove.c

---
+++ ft_memmove.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abayram <abayram@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/20 20:30:22 by abayram           #+#    #+#             */
+/*   Updated: 2026/01/20 21:07:19 by abayram          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+void	*memmove(void *target, const void *head, size_t count)
+{
+	unsigned char	*char_target;
+	unsigned char	*char_head;
+
+	if (target == NULL && head == NULL)
+	{
+		return (NULL);
+	}
+	char_target = (unsigned char *)target;
+	char_head = (unsigned char *)head;
+	if (char_target > char_head)
+	{
+		while (count--)
+		{
+			char_target[count] = char_head[count];
+		}
+	}
+	else
+	{
+		while (count--)
+		{
+			*char_target++ = *char_head++;
+		}
+	}
+	return (target);
+}
