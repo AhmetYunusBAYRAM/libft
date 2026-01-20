fb9ef2ae88e2a186e7795c215b1f6bb9fdd5c594
Ahmet Yunus Bayram <abayram@k1m13s04.42kocaeli.com.tr>
2026-01-20 22:52:01 +0300

    ft_strlcpy.c

---
+++ ft_strlcpy.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abayram <abayram@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/20 22:32:16 by abayram           #+#    #+#             */
+/*   Updated: 2026/01/20 22:51:11 by abayram          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+size_t	ft_strlcpy(char *target, const char *head, size_t count)
+{
+	size_t	target_count;
+	size_t	index;
+
+	target_count = 0;
+	index = 0;
+	while (head[target_count] != '\0')
+	{
+		target_count++;
+	}
+	if (count == 0)
+	{
+		return (target_count);
+	}
+	while (head[index] != '\0' && index < (count - 1))
+	{
+		target[index] = head[index];
+		index++;
+	}
+	target[index] = '\0';
+	return (target_count);
+}
