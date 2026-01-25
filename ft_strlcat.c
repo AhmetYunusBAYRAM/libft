73daa8e1c6f7c1ba80af9edd919cea0ed7951fcc
Ahmet Yunus Bayram <abayram@k2m14s06.42kocaeli.com.tr>
2026-01-25 18:49:35 +0300

    ft_strlcat.c geliştirilmesi yapıldı

---
+++ ft_strlcat.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abayram <abayram@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/25 18:40:01 by abayram           #+#    #+#             */
+/*   Updated: 2026/01/25 18:49:13 by abayram          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	index;
+
+	index = 0;
+	while (str[index])
+	{
+		index ++;
+	}
+	return (index);
+}
+
+size_t	fr_strlcat(char *target, const char *head, size_t count)
+{
+	size_t	target_len;
+	size_t	head_len;
+	size_t	index_first;
+	size_t	index_second;
+
+	target_len = ft_strlen(head);
+	head_len = ft_strlen(target);
+	if (count <= target_len)
+		return (count + target_len);
+	index_first = head_len;
+	index_second = 0;
+	while (head[index_second] && (index_first + 1) < count)
+	{
+		target[index_first] = head[index_second];
+		index_first++;
+		index_second++;
+	}
+	target[index_first] = '\0';
+	return (target_len + head_len);
+}
