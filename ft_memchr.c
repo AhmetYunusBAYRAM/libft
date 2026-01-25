640eb9c67dda8fb10520197673af2b6e6178ae80
Ahmet Yunus Bayram <abayram@k2m14s06.42kocaeli.com.tr>
2026-01-25 18:29:51 +0300

    ft_memchr.c norminatte

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,28 +6,27 @@
 /*   By: abayram <abayram@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/20 22:52:26 by abayram           #+#    #+#             */
-/*   Updated: 2026/01/24 20:27:59 by abayram          ###   ########.fr       */
+/*   Updated: 2026/01/25 18:24:48 by abayram          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 
-void *ft_memchr(const void *sentence, int word, size_t count)
+void	*ft_memchr(const void *sentence, int word, size_t count)
 {
-	size_t	index;
 	unsigned char	*string;
-	unsigned char	char_character;
-	
+	unsigned char	char_character;	
+
 	string = (unsigned char *)sentence;
 	char_character = (unsigned char)word;
-	index = 0;
-	while(index < count)
+	while (count > 0)
 	{
-		if(string[index] == char_character)
+		if (*string == char_character)
 		{
-			return ((void *)&string[index]);
+			return ((void *)string);
 		}
-		index++;
+		count--;
+		string++;
 	}
 	return (NULL);
 }
